7161b3a3444edbc2f87ff1977f3e0d94df51489d
technicalaks <105633032+technicalaks@users.noreply.github.com>
2022-09-11 09:25:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -755,23 +755,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('♻️ ABOUT ♻️', callback_data='about')
             ],[
             InlineKeyboardButton('✅ SUBSCᏒIBΞ  ✅', url='https://youtube.com/channel/UCPWbsW-g2CW5PSJ_50CmtUQ')
-        ]]
-        reply1 = await query.message.reply_text(
-            text="wait..."
-        )
-        await asyncio.sleep(0.5)
-        reply2 = await reply1.edit_text(
-            text="wait..."
-        )
-        await asyncio.sleep(0.5)
-        reply3 = await reply2.edit_text(
-            text="wait..."
-        )
-        await asyncio.sleep(0.5)
-        reply4 = await reply3.edit_text(
-            text="wait..."
-        )
-        await reply4.delete()
+        ]]        
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
             text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
